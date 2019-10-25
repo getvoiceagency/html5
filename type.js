988b3c818af4e5ce71ddf3a08072c7866a68b056
john@getvoice.agency <john@getvoice.agency>
2019-10-25 13:05:45 +0000

    ai with Basebinary

--- type.js
+++ type.js
@@ -1,35 +1,91 @@
 "use strict";
+//Base Binary Value 50/50
+var BinBase = 50;
+//Set Difficulty Modifier
+var easy = 45;
+var medium = 35;
+var hard = 25;
+var outPut = "over";
+// Set Card Variables
 var star = 1;
 var heart = 8;
 var defence = 3;
 var blue = 6;
 var orange = 1;
 var green = 4;
-var resultPower = Math.max(star, heart, defence);
-var resultTwist = Math.max(blue, orange, green);
-if (star = resultPower) {
-    var emPower = resultPower;
-    console.log(({ emPower: emPower }));
+//Create the beat Me score
+var beatMe = BinBase + hard;
+//Roll a D100
+var diceRoll = Math.floor(Math.random() * 100) + 1;
+//If diceRoll is less than beat me Success
+if (diceRoll < beatMe) {
+    // Find which stat is greatest and store it
+    var resultPower = Math.max(star, heart, defence);
+    var resultTwist = Math.max(blue, orange, green);
+    //Find which power was the greater and add it's value to emPower
+    if (star = resultPower) {
+        var emPower = resultPower;
+        console.log(({ emPower: emPower }));
+    }
+    else if (heart = resultPower) {
+        var emPower = resultPower;
+        console.log(({ emPower: emPower }));
+    }
+    else if (defence = resultPower) {
+        var emPower = resultPower;
+        console.log(({ emPower: emPower }));
+    }
+    //Find which twist was the greater and add it's value to emTwist
+    if (star = resultTwist) {
+        var emTwist = resultTwist;
+        console.log(({ emTwist: emTwist }));
+    }
+    else if (heart = resultTwist) {
+        var emTwist = resultTwist;
+        console.log(({ emTwist: emTwist }));
+    }
+    else if (defence = resultTwist) {
+        var emTwist = resultTwist;
+        console.log(({ emTwist: emTwist }));
+    }
+    //Console and do Next
+    console.log(({ resultPower: resultPower }));
+    console.log(({ resultTwist: resultTwist }));
+    console.log(({ beatMe: beatMe }));
 }
-else if (heart = resultPower) {
-    var emPower = resultPower;
-    console.log(({ emPower: emPower }));
+else if (diceRoll > beatMe) {
+    // Find which stat is greatest and store it
+    var resultPower = Math.max(star, heart, defence);
+    var resultTwist = Math.max(blue, orange, green);
+    //Find which power was the greater and add it's value to emPower
+    if (star = resultPower) {
+        var emPower = resultPower;
+        console.log(({ emPower: emPower }));
+    }
+    else if (heart = resultPower) {
+        var emPower = resultPower;
+        console.log(({ emPower: emPower }));
+    }
+    else if (defence = resultPower) {
+        var emPower = resultPower;
+        console.log(({ emPower: emPower }));
+    }
+    //Find which twist was the greater and add it's value to emTwist
+    if (star = resultTwist) {
+        var emTwist = resultTwist;
+        console.log(({ emTwist: emTwist }));
+    }
+    else if (heart = resultTwist) {
+        var emTwist = resultTwist;
+        console.log(({ emTwist: emTwist }));
+    }
+    else if (defence = resultTwist) {
+        var emTwist = resultTwist;
+        console.log(({ emTwist: emTwist }));
+    }
+    //Console and do Next
+    console.log(({ resultPower: resultPower }));
+    console.log(({ resultTwist: resultTwist }));
+    console.log(({ beatMe: beatMe }));
+    console.log(({ outPut: outPut }));
 }
-else if (defence = resultPower) {
-    var emTwist = resultPower;
-    console.log(({ emTwist: emTwist }));
-}
-if (star = resultTwist) {
-    var emTwist = resultTwist;
-    console.log(({ emTwist: emTwist }));
-}
-else if (heart = resultTwist) {
-    var emTwist = resultTwist;
-    console.log(({ emTwist: emTwist }));
-}
-else if (defence = resultTwist) {
-    var emTwist = resultTwist;
-    console.log(({ emTwist: emTwist }));
-}
-console.log(({ resultPower: resultPower }));
-console.log(({ resultTwist: resultTwist }));
--- type.ts
+++ type.ts
@@ -1,3 +1,14 @@
+
+//Base Binary Value 50/50
+let BinBase = 50
+//Set Difficulty Modifier
+let easy = 45
+let medium = 35
+let hard =25
+let outPut = "over"
+
+
+// Set Card Variables
 let star = 1
 let heart = 8
 let defence = 3
@@ -5,9 +16,22 @@ let blue = 6
 let orange =1
 let green =4
 
+//Create the beat Me score
+let beatMe = BinBase+hard 
+
+//Roll a D100
+let diceRoll = Math.floor(Math.random() * 100) + 1  
+
+//If diceRoll is less than beat me Success
+if (diceRoll < beatMe )
+
+{
+    
+// Find which stat is greatest and store it
 let resultPower = Math.max(star,heart,defence)
 let resultTwist = Math.max(blue,orange,green)
 
+//Find which power was the greater and add it's value to emPower
 if 
 (star = resultPower )
 {
@@ -23,10 +47,10 @@ else if
 
 else if 
 (defence = resultPower) {
-    let emTwist = resultPower
-    console.log(( {emTwist} ) );
+    let emPower = resultPower
+    console.log(( {emPower} ) );
 }    
-
+//Find which twist was the greater and add it's value to emTwist
 if 
 (star = resultTwist )
 {
@@ -44,9 +68,69 @@ else if
 (defence = resultTwist) {
     let emTwist = resultTwist
     console.log(( {emTwist} ) );
-}    
+}
 
 
-
+//Console and do Next
 console.log(( {resultPower} ) );
 console.log(( {resultTwist} ) );
+console.log(( {beatMe} ) );
+}
+else if (diceRoll > beatMe)
+
+{
+    
+    
+// Find which stat is greatest and store it
+let resultPower = Math.max(star,heart,defence)
+let resultTwist = Math.max(blue,orange,green)
+
+//Find which power was the greater and add it's value to emPower
+if 
+(star = resultPower )
+{
+    let emPower = resultPower
+   console.log(( {emPower} ) );
+}
+
+else if
+(heart = resultPower) {
+    let emPower = resultPower
+   console.log(( {emPower} ) );
+}    
+
+else if 
+(defence = resultPower) {
+    let emPower = resultPower
+    console.log(( {emPower} ) );
+}    
+//Find which twist was the greater and add it's value to emTwist
+if 
+(star = resultTwist )
+{
+    let emTwist = resultTwist
+   console.log(( {emTwist} ) );
+}
+
+else if
+(heart = resultTwist) {
+    let emTwist = resultTwist
+   console.log(( {emTwist} ) );
+}    
+
+else if 
+(defence = resultTwist) {
+    let emTwist = resultTwist
+    console.log(( {emTwist} ) );
+}
+
+
+//Console and do Next
+console.log(( {resultPower} ) );
+console.log(( {resultTwist} ) );
+console.log(( {beatMe} ) );
+console.log(( {outPut} ) );
+}
+
+
+
