f5aea796e83b43cbefef8ea9b908238822cff338
john@getvoice.agency <john@getvoice.agency>
2019-11-01 09:09:41 +0000

    menu.html

--- cockpit.js
+++ cockpit.js
@@ -3,19 +3,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var obj;
 var cross_fetch_1 = __importDefault(require("cross-fetch"));
 cross_fetch_1.default('https://flightdeck.getvoice.agency/api/collections/get/CardStarterDeck/posts?token=427ae196a562ea77d85db712f2a2f2', {
     method: 'post',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
         filter: { published: true },
-        fields: { fieldA: 1, fieldB: 1 },
-        limit: 10,
-        skip: 5,
-        sort: { _created: -1 },
+        filterParameters: { "id": 1 },
+        fields: { cardName: 1, cardImage: 1 },
         populate: 1,
-        lang: 'de' // return normalized language fields (fieldA_de => fieldA)
     })
 })
     .then(function (res) { return res.json(); })
-    .then(function (res) { return console.log(res); });
+    .then(function (collection) { return console.log(collection); });
+var content = collection.cardName;
+console.log(obj);
--- cockpit.ts
+++ cockpit.ts
@@ -1,17 +1,19 @@
+let obj
+
 import fetch from 'cross-fetch';
     fetch('https://flightdeck.getvoice.agency/api/collections/get/CardStarterDeck/posts?token=427ae196a562ea77d85db712f2a2f2', {
     method: 'post',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
         filter: {published:true},
-        fields: {fieldA: 1, fieldB: 1},
-        limit: 10,
-        skip: 5,
-        sort: {_created:-1},
-        populate: 1, // resolve linked collection items
-
-        lang: 'de' // return normalized language fields (fieldA_de => fieldA)
+        filterParameters: {"id":1},
+        fields: {cardName: 1, cardImage: 1},
+        populate: 1,
     })
 })
-.then(res=>res.json())
-.then(res => console.log(res));
+    .then(res => res.json())
+    .then(collection => console.log(collection));
+
+console.log(obj);
+    
+
